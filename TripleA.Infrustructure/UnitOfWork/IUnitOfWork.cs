366710cde024464f4e284216024eb94cf1593f8c
Ahmed Reda <95850640+AhmedReda103@users.noreply.github.com>
2024-06-11 20:32:26 +0300

    addUnitOfWork

---
+++ TripleA.Infrustructure/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using TripleA.Infrustructure.Context;
+
+namespace TripleA.Infrustructure.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        ApplicationDbContext _context { get; }
+
+        Task SaveChangesAsync();
+    }
+
+
+}
---
+++ TripleA.Infrustructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using TripleA.Infrustructure.Context;
+
+namespace TripleA.Infrustructure.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public ApplicationDbContext _context { get; set; }
+
+
+        public UnitOfWork(
+          ApplicationDbContext context
+        )
+        {
+            _context = context;
+        }
+
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
