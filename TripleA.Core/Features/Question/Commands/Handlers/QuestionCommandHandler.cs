01295189367764a2bb921e3efd28a6a6b553a46f
DESKTOP-OD70FRL\mohamed kadry <muhammadkadry22@gmail.com>
2024-06-14 15:53:56 +0300

    add FKs to user from Question,answer,comment

---
+++ TripleA.Core/Features/Question/Commands/Handlers/QuestionCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TripleA.Core.Bases;
+using TripleA.Core.Features.Question.Commands.Models;
+using TripleA.Service.Abstracts;
+
+namespace TripleA.Core.Features.Question.Commands.Handlers
+{
+    public class QuestionCommandHandler : ResponseHandler//,
+                                     //     IRequestHandler<AddQuestionCommand, Response<string>>
+    {
+        private readonly IMapper mapper;
+        private readonly IQuestionService questionService;
+
+        public QuestionCommandHandler(IMapper mapper,IQuestionService questionService)
+        {
+            this.mapper = mapper;
+            this.questionService = questionService;
+        }
+        //public async Task<Response<string>> Handle(AddQuestionCommand request, CancellationToken cancellationToken)
+        //{
+            
+        //}
+    }
+}
