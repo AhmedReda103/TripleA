3052218c99c67b3dc6f5eac57eb6aa89b9dbea4e
Ahmed Reda <95850640+AhmedReda103@users.noreply.github.com>
2024-06-25 00:38:26 +0300

    cashing request data in reddis  to question by id and comments

--- TripleA.Core/Features/Comment/Queries/Handler/CommentQueryHandler.cs
+++ TripleA.Core/Features/Comment/Queries/Handler/CommentQueryHandler.cs
@@ -12,17 +12,26 @@ namespace TripleA.Core.Features.Comment.Queries.Handler
     {
         private readonly IMapper mapper;
         private readonly ICommentService commentService;
-        public CommentQueryHandler(IMapper mapper, ICommentService commentService)
+        private readonly ICashingService cashingService;
+        public CommentQueryHandler(IMapper mapper, ICommentService commentService, ICashingService cashingService)
         {
             this.mapper = mapper;
             this.commentService = commentService;
+            this.cashingService = cashingService;
         }
 
         public async Task<Response<GetCommentByIdDto>> Handle(GetCommentByIdQuery request, CancellationToken cancellationToken)
         {
+            var key = "GetQuestionsByIdQuery" + request.Id;
+            var cashingData = await cashingService.GetData<GetCommentByIdDto>(key);
+            /* if (cashingData != null)
+             {
+                 return Success(cashingData);
+             }*/
             var comment = await commentService.GetCommentByIDAsync(request.Id);
             if (comment == null) return NotFound<GetCommentByIdDto>();
             var commentMapper = mapper.Map<GetCommentByIdDto>(comment);
+            //cashingService.setData(key, commentMapper, TimeSpan.FromMinutes(1));
             var result = Success(commentMapper);
             return result;
 
