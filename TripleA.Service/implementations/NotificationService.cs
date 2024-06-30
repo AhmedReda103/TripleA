0034f8100731901df7f5056205d212b301c5c403
Ahmed Reda <95850640+AhmedReda103@users.noreply.github.com>
2024-06-30 20:01:04 +0300

    add UpdateReadNotifications EndPoint

--- TripleA.Service/implementations/NotificationService.cs
+++ TripleA.Service/implementations/NotificationService.cs
@@ -1,4 +1,5 @@
-﻿using TripleA.Data.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using TripleA.Data.Entities;
 using TripleA.Infrustructure.unitOfWork;
 using TripleA.Service.Abstracts;
 
@@ -34,5 +35,26 @@ namespace TripleA.Service.implementations
             var notifications = await unitOfWork.Notifications.GetAllAsync();
             return notifications.Where(n => n.UserId == userId).OrderByDescending(n => n.CreatedIn).ToList();
         }
+
+        public async Task<string> UpdateReadNotificationAsync()
+        {
+            try
+            {
+                var notifications = await unitOfWork.Notifications.GetAllAsync();
+                foreach (var notification in notifications)
+                {
+                    notification.IsRead = true;
+                    unitOfWork._context.Entry(notification).State = EntityState.Modified;
+                }
+                await unitOfWork.SaveChangesAsync();
+                return "Updated";
+            }
+            catch (Exception ex)
+            {
+                return "updating Notification Failed " + ex.Message;
+            }
+        }
+
+
     }
 }
