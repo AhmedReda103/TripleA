edf8546402dce6dfc76109301f4b56d6120a2bd6
DESKTOP-OD70FRL\mohamed kadry <muhammadkadry22@gmail.com>
2024-07-01 01:03:04 +0300

    showing questions of user in userProfile

---
+++ TripleA.Core/Features/ApplicationUser/Queries/Dtos/UserProfileQuestionsDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripleA.Core.Features.ApplicationUser.Queries.Dtos
+{
+    public class UserProfileQuestionsDto
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public string? Title { get; set; }
+        public string? Image { get; set; }
+        public DateTime? CreatedIn { get; set; } 
+    }
+}
